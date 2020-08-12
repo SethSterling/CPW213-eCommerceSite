1d33b8fb9800fb6d153a70b6d6c93171b9617253
SethSterling <63669378+SethSterling@users.noreply.github.com>
2020-08-12 16:07:26 -0700

    Issue#23 refactor (#25)
    * Styled add product page.
    
    * Revert "Styled add product page."
    
    This reverts commit 7ce31cd59d85eda21783f5952025d924600c9dbe.
    
    * Refactor Db code into separate class

---
+++ eCommerceSite/Data/ProductDB.cs
@@ -0,0 +1,47 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Data
+{
+    public static class ProductDB
+    {
+        /// <summary>
+        /// Returns the total count of products
+        /// </summary>
+        /// <param name="_context">Database context to use</param>
+        public async static Task<int> GetTotalProductsAsync(ProductContext _context)
+        {
+            return await (from p in _context.products
+                   select p).CountAsync();
+        }
+
+        /// <summary>
+        /// Get a page worth of products
+        /// </summary>
+        /// <param name="_context">Database context to use</param>
+        /// <param name="pageSize">The number of products per page</param>
+        /// <param name="pageNum">Page of products to return</param>
+        /// <returns></returns>
+        public static async Task<List<Product>> GetProductsAsync(ProductContext _context, int pageSize, int pageNum)
+        {
+           return await (from p in _context.products
+                   orderby p.Title ascending
+                   select p)
+                   .Skip(pageSize * (pageNum - 1))
+                   .Take(pageSize)
+                   .ToListAsync();
+        }
+
+        public static async Task<Product> AddProductAsync(ProductContext _context, Product product)
+        {
+            //Add to DB
+            _context.products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+}
