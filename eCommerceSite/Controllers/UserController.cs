021c654951783a65b1801c8a311c9d62d9b2f3f8
SethSterling <sethbdsterling@gmail.com>
2020-08-19 17:59:40 -0700

    Added registered user into DB

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceSite.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ProductContext _context;
+
+        public UserController(ProductContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map data to user account instance
+                UserAccount account = new UserAccount()
+                {
+                    DateOfBirth = reg.DateOfBirth,
+                    Email = reg.Email,
+                    Password = reg.Password,
+                    Username = reg.Username
+                };
+                //Add to database
+                _context.UserAccounts.Add(account);
+                await _context.SaveChangesAsync();
+                //Redirect to home page
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(reg);
         }
     }
 }
