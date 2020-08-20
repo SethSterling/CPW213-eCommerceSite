3b4ac88fcdae9a9c1b346df9326668e571d2264d
SethSterling <sethbdsterling@gmail.com>
2020-08-19 17:40:38 -0700

    Added a register page and more validation

--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -22,19 +22,25 @@ namespace eCommerceSite.Models
     public class RegisterViewModel
     {
         [Required]
+        [EmailAddress]
+        [StringLength(200)]
         public string Email { get; set; }
 
-        [Compare(nameof(Email))]
         [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
         public string ConfirmEmail { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between {2} and {1}")]
         public string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        [StringLength(120, MinimumLength = 6)]
         public string ConfirmPassword { get; set; }
 
         [DataType(DataType.Date)]
