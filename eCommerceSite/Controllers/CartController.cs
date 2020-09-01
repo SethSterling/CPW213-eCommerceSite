b09c043c8818bd8c9f95760c8f3896edc39f2a84
SethSterling <sethbdsterling@gmail.com>
2020-09-01 16:56:56 -0700

    Redirect to previous page when adding a product to cart

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -25,7 +25,7 @@ namespace eCommerceSite.Controllers
         /// </summary>
         /// <param name="id">The id of the product to add</param>
         /// <returns></returns>
-        public async Task<IActionResult> Add(int id)
+        public async Task<IActionResult> Add(int id, string prevUrl)
         {
             // Get product from the database
             Product p = await ProductDB.GetProductAsync(_context, id);
@@ -36,7 +36,7 @@ namespace eCommerceSite.Controllers
             TempData["Message"] = $"{p.Title} was added to your cart";
 
             // Redirect back to previous page
-            return RedirectToAction("Index", "Product");
+            return Redirect(prevUrl);
         }
 
         public IActionResult Summary()
